6eb6228faf98d8f7c9f76f37e28bf7853770adc6
Bulgrak <erik.gravesen@gmail.com>
2013-08-01 18:25:12 +0200

    UI

---
+++ LoanApp/LoanApp/Model/Loan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApp.Model
+{
+    class Loan
+    {
+        public List<Loan> MyLoans { get; set; }
+        public string gert;
+        public string Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime HandInDate { get; set; }
+
+        public Loan()
+        {
+            MyLoans = new List<Loan>();
+        }
+
+        public string Gert
+        {
+            get {return gert;}
+            set { gert = value;}
+        }
+    }
+}
